932a6ae638e72e403cdab69ea1b7f0b3d4f96021
Lucio Franco <luciofranco14@gmail.com>
2025-05-04 13:48:42 -0400

    add print2 message
    Test Plan: foobarbaz
    
    Pull Request: https://github.com/LucioFranco/stacked-prs-test/pull/3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ fn main() {
     hello_world();
 }
 
-fn print_hello_world2() {}
+fn print_hello_world2() {
+    println!("hello world")
+}
 
 fn hello_world() {
     println!("some other hello world");
